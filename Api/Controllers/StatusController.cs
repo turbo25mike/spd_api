817394cd5ca9eb07956edd2199e65a43436de582
turbo25mike <turbo25mike@gmail.com>
2017-01-27 14:05:54 -0800

    env test

--- Api/Controllers/StatusController.cs
+++ Api/Controllers/StatusController.cs
@@ -1,13 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Configuration;
 using System.Linq;
 using System.Net.Http;
 using System.Security.Claims;
 using System.Web.Http;
-using Amazon.EC2.Model;
 using Microsoft.AspNet.Identity;
-using Amazon.EC2;
 
 namespace Api.Controllers
 {
@@ -23,7 +20,8 @@ namespace Api.Controllers
         [Route("environment")]
         public HttpResponseMessage GetEnvironment()
         {
-            return Request.CreateResponse(ConfigurationManager.AppSettings["ENVIRONMENT"]);
+            var results = string.Join(",", ConfigurationManager.AppSettings.AllKeys.Select(key => ConfigurationManager.AppSettings[key]));
+            return Request.CreateResponse(results);
         }
 
         [Route("exception")]
@@ -38,23 +36,6 @@ namespace Api.Controllers
         {
             var userId = ClaimsPrincipal.Current.Identity.GetUserId();
             return Request.CreateResponse($"Hello, {userId}! You are currently authenticated.");
-        }
-
-        [Route("tags")]
-        public HttpResponseMessage GetTags()
-        {
-            var client = new AmazonEC2Client();
-            var tags = new List<string>();
-            var request = new DescribeInstancesRequest();
-            var response = client.DescribeInstances(request);
-            foreach (var ec2 in response.Reservations)
-            {
-                foreach (var instance in ec2.Instances)
-                {
-                    tags.AddRange(instance.Tags.Select(t => t.Key));
-                }
-            }
-            return Request.CreateResponse(string.Join(",", tags));
         }
     }
 }
