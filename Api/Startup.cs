06d124dffaa846f8cdeea1efe36d9851f71f351e
turbo25mike <turbo25mike@gmail.com>
2017-01-30 13:21:08 -0800

    break fix

--- Api/Startup.cs
+++ Api/Startup.cs
@@ -1,5 +1,10 @@
-﻿using System.Web.Http;
+﻿using System;
+using System.IdentityModel.Tokens;
+using System.Web.Http;
+using Auth0.Owin;
 using Microsoft.Owin.Cors;
+using Microsoft.Owin.Security;
+using Microsoft.Owin.Security.Jwt;
 using Owin;
 
 namespace Api
@@ -9,10 +14,24 @@ namespace Api
         public void Configuration(IAppBuilder appBuilder)
         {
             appBuilder.UseCors(CorsOptions.AllowAll); //must be first
+            var audience = Environment.GetEnvironmentVariable("AUTH0_CLIENT_IDS") ?? "VRuQOIlWkBs3WEwjwafACwuY43tWZ5Tn";
+            var domain = Environment.GetEnvironmentVariable("AUTH0_DOMAIN") ?? "https://spd.auth0.com/";
+            var keyResolver = new OpenIdConnectSigningKeyResolver(domain);
+            appBuilder.UseJwtBearerAuthentication(
+                new JwtBearerAuthenticationOptions
+                {
+                    AuthenticationMode = AuthenticationMode.Active,
+                    TokenValidationParameters = new TokenValidationParameters()
+                    {
+                        ValidAudience = audience,
+                        ValidIssuer = domain,
+                        IssuerSigningKeyResolver = (token, securityToken, identifier, parameters) => keyResolver.GetSigningKey(identifier)
+                    }
+                });
             var httpConfiguration = new HttpConfiguration();
             httpConfiguration.MapHttpAttributeRoutes();
             UnityConfig.Register(httpConfiguration);
-            Auth0Config.Register(appBuilder);
+            //Auth0Config.Register(appBuilder);
             appBuilder.UseWebApi(httpConfiguration);
         }
     }
