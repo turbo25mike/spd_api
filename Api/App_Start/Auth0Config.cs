01f3550df0cc0bf8080a8bb442a808edd5aa338c
turbo25mike <turbo25mike@gmail.com>
2017-01-30 12:34:50 -0800

    change to startup

---
+++ Api/App_Start/Auth0Config.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IdentityModel.Tokens;
+using Auth0.Owin;
+using Microsoft.Owin.Security;
+using Microsoft.Owin.Security.Jwt;
+using Owin;
+
+namespace Api
+{
+    public static class Auth0Config
+    {
+        public static void Register(IAppBuilder appBuilder)
+        {
+            var audience = Environment.GetEnvironmentVariable("AUTH0_CLIENT_IDS") ?? "VRuQOIlWkBs3WEwjwafACwuY43tWZ5Tn";
+            var domain = Environment.GetEnvironmentVariable("AUTH0_DOMAIN") ?? "https://spd.auth0.com/";
+            var keyResolver = new OpenIdConnectSigningKeyResolver(domain);
+            appBuilder.UseJwtBearerAuthentication(
+                new JwtBearerAuthenticationOptions
+                {
+                    AuthenticationMode = AuthenticationMode.Active,
+                    TokenValidationParameters = new TokenValidationParameters()
+                    {
+                        ValidAudience = audience,
+                        ValidIssuer = domain,
+                        IssuerSigningKeyResolver = (token, securityToken, identifier, parameters) => keyResolver.GetSigningKey(identifier)
+                    }
+                });
+        }
+    }
+}
--- Api/Startup.cs
+++ Api/Startup.cs
@@ -1,10 +1,5 @@
-﻿using System;
-using System.IdentityModel.Tokens;
-using System.Web.Http;
-using Auth0.Owin;
+﻿using System.Web.Http;
 using Microsoft.Owin.Cors;
-using Microsoft.Owin.Security;
-using Microsoft.Owin.Security.Jwt;
 using Owin;
 
 namespace Api
@@ -14,26 +9,12 @@ namespace Api
         public void Configuration(IAppBuilder appBuilder)
         {
             appBuilder.UseCors(CorsOptions.AllowAll); //must be first
-
-            var domain = Environment.GetEnvironmentVariable("AUTH0_DOMAIN") ?? "";
-            var audience = Environment.GetEnvironmentVariable("AUTH0_CLIENT_IDS") ?? "";
-            var keyResolver = new OpenIdConnectSigningKeyResolver(domain);
-            appBuilder.UseJwtBearerAuthentication(
-                new JwtBearerAuthenticationOptions
-                {
-                    AuthenticationMode = AuthenticationMode.Active,
-                    TokenValidationParameters = new TokenValidationParameters()
-                    {
-                        ValidAudience = audience,
-                        ValidIssuer = domain,
-                        IssuerSigningKeyResolver = (token, securityToken, identifier, parameters) => keyResolver.GetSigningKey(identifier)
-                    }
-                });
-
             var httpConfiguration = new HttpConfiguration();
             httpConfiguration.MapHttpAttributeRoutes();
+            Auth0Config.Register(appBuilder);
             UnityConfig.Register(httpConfiguration);
             appBuilder.UseWebApi(httpConfiguration);
+            
         }
     }
 }
